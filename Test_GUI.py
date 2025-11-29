ef77ca33e710998466bfec59bbf91ac1c34f8466
Janessa Cassandra Thomas <janessacassandra@gmail.com>
2025-11-28 23:43:27 -0600

    Adding tabbed interface using ttk.Notebook

--- Test_GUI.py
+++ Test_GUI.py
@@ -10,25 +10,33 @@ root = tk.Tk()
 root.geometry("360x600")
 root.title("Bunny Weather App V.3")
 
-# Tab Home
-# Tab Report Weather
-# Tab Your Report's
-# Tab Settings
-# tab About Application 
-
-# Creating tabs
+# widget setup
+# Creating tab control
 tab_control = ttk.Notebook(root)
+
+# Creating tabs 5 in total
+# Tab Home
 tab1 = ttk.Frame(tab_control)
+label1 = ttk.Label(tab1, text = "Home")
+
+# Tab Your Report's
 tab2 = ttk.Frame(tab_control)
+label2 = ttk.Label(tab2, text = "Report Weather")
+
+# Tab Report Weather
 tab3 = ttk.Frame(tab_control)
+label3 = ttk.Label(tab3, text = "Your Report's")
+
+# Tab Settings
 tab4 = ttk.Frame(tab_control)
+label4 = ttk.Label(tab4, text = "Settings")
+
+# tab About Application
 tab5 = ttk.Frame(tab_control)
-tab_control.add(tab1, text='Home')
-tab_control.add(tab2, text='Report Weather')
-tab_control.add(tab3, text="Your Report's")
-tab_control.add(tab4, text='Settings')  
-tab_control.add(tab5, text='About')
-tab_control.pack(expand=1, fill='both')
+label5 = ttk.Label(tab5, text = "About")
+
+# package tab control
+tab_control.pack()
 
 # run the application
 root.mainloop()
