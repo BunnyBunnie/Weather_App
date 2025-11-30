6258f9e14e61c5c10db1ab99a299994a38c25c85
Janessa Cassandra Thomas <janessacassandra@gmail.com>
2025-11-29 22:27:54 -0600

    fixed tkk labels

--- Test_GUI.py
+++ Test_GUI.py
@@ -17,23 +17,23 @@ tab_control = ttk.Notebook(root)
 # Creating tabs 5 in total
 # Tab Home
 tab1 = ttk.Frame(tab_control)
-label1 = ttk.Label(tab1, text = "Home")
+tab_control.add(tab1, text = "Home")
 
 # Tab Your Report's
 tab2 = ttk.Frame(tab_control)
-label2 = ttk.Label(tab2, text = "Report Weather")
+tab_control.add(tab2, text = "Report Weather")
 
 # Tab Report Weather
 tab3 = ttk.Frame(tab_control)
-label3 = ttk.Label(tab3, text = "Your Report's")
+tab_control.add(tab3, text = "Your Report's")
 
 # Tab Settings
 tab4 = ttk.Frame(tab_control)
-label4 = ttk.Label(tab4, text = "Settings")
+tab_control.add(tab4, text = "Settings")
 
 # tab About Application
 tab5 = ttk.Frame(tab_control)
-label5 = ttk.Label(tab5, text = "About")
+tab_control.add(tab5, text= "About")
 
 # package tab control
 tab_control.pack()
